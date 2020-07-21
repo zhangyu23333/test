7d0ff36db7cbc9bcafdab621183d5e225003addd
zhangyu23333 <1435764331@qq.com>
2020-07-21 12:20:03 +0800

    张瑜

---
+++ pages/login/login.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+
+
+Page({
+  data: {
+    // kino: '',
+  },
+  
+  // getkino() {
+
+  //   this.setData({
+  //     kino: 'test',
+  //   })
+  // },
+  // onLoad() {
+  //   this.getkino()
+  // }
+})
